ff87e8339ea17c23cdbe19057d1b182304c037e2
Elizabeth <elizabethbarbosa691@gmail.com>
2025-11-16 03:16:47 -0300

    feat: backend structure with prisma and docker

---
+++ backend/src/types/fastify-jwt.d.ts
@@ -0,0 +1,15 @@
+import '@fastify/jwt'
+
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
+    payload: {
+      name: string
+      email: string
+    }
+    user: {
+      sub: string
+      name: string
+      email: string
+    }
+  }
+}
